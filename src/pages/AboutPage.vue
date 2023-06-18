38b255d039a1604ac4c41bae1708d23ff6e257e0
ArielDawidowicz <arieldawidowicz1@gmail.com>
2023-06-18 13:13:27 +0300

    about page design

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,81 +1,150 @@
+
 <template>
-    <div class="container">
-      <h1 class="mt-4">About Us</h1>
-      <b-row>
-        <b-col md="6">
-          <h2 class="mt-4">Flavor Fiesta Website</h2>
-          <p>Welcome to our Flavor Fiesta Website! </p>
-        </b-col>
-      </b-row>
-      
-      <b-row>
-        <b-col md="6">
-          <h2 class="mt-4">Our Team</h2>
-          <b-list-group>
-            <b-list-group-item v-for="member in teamMembers" :key="member.id">
-              <h3>{{ member.name }}</h3>
-              <p>{{ member.role }}</p>
-              <p>{{ member.description }}</p>
-            </b-list-group-item>
-          </b-list-group>
-        </b-col>
-      </b-row>
-      
-      <b-row>
-        <b-col md="6">
-          <h2 class="mt-4">Previous Projects</h2>
-          <b-list-group>
-            <b-list-group-item v-for="project in previousProjects" :key="project.id">
-              <h3>{{ project.title }}</h3>
-              <p>{{ project.description }}</p>
-              <b-button :href="project.link" target="_blank" variant="primary">Go To Project</b-button>
-            </b-list-group-item>
-          </b-list-group>
-        </b-col>
-      </b-row>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <h2 id="title" class="text-center">About Us</h2>
+        <div class="w-75 mx-auto">
+          <p class="text-center mt-4">
+            Welcome to Flavor Fiesta, your ultimate destination for delicious
+            food recipes! Whether you're a seasoned chef or a beginner in the
+            kitchen, we have a wide range of recipes to satisfy your taste buds.
+            From mouthwatering appetizers to delectable desserts, our collection
+            will inspire you to create culinary masterpieces in your own home.
+            Join us on this flavor-filled journey and let your taste buds dance
+            with joy!
+          </p>
+        </div>
+      </div>
     </div>
-  </template>
+
+    <div class="row mt-5">
+      <div class="col">
+        <h2 id="title" class="text-center">Our Team</h2>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col-md-6 d-flex align-items-center justify-content-center">
+        <div class="card">
+          <img
+            :src="ariel"
+            id="team_image"
+            class="card-img-top mx-auto"
+            alt="Ariel Dawidowicz"
+          />
+          <div class="card-body">
+            <h5 class="card-title text-center font-weight-light mb-4 mt-2">
+              Ariel Dawidowicz
+            </h5>
+            <p class="card-text">
+              Ariel is a passionate food enthusiast.
+              With years of experience in the culinary
+              world, Ariel brings a unique blend of flavors to our recipes. From
+              traditional family recipes to innovative creations.
+            </p>
+            <h6 class="mt-5" style="text-decoration: underline;">Last Project</h6>
+            <b>Personal Site - Andras Arato</b>
+            <p class="project-description">
+              A personal site featuring Andras Arato, also known as "Hide the Pain Harold," a famous meme.
+              This project, created by Ariel, showcases Andras Arato's story.
+
+            </p>
+            <b-button href="https://web-development-environments-2023.github.io/324474857" size="sm" class=" w-25" variant="success">View Project</b-button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 d-flex align-items-center justify-content-center">
+        <div class="card">
+          <img
+            :src="eithan"
+            id="team_image"
+            class="card-img-top  mx-auto"
+            alt="Eithan Markman"
+          />
+          <div class="card-body">
+            <h5 class="card-title text-center font-weight-light mb-4 mt-2">
+              Eithan Markman
+            </h5>
+            <p class="card-text">
+              Eithan is a culinary prodigy with a knack for experimentation. His
+              inventive recipes push the boundaries of traditional cooking and
+              offer a fresh perspective on flavor combinations. Eithan's
+              creativity and attention to detail make him an invaluable member
+              of our team.
+            </p>
+            <h6 class="mt-5" style="text-decoration: underline;">Last Project</h6>
+            <b>Personal Site - Kareem Abdul Jabbar</b>
+            <p class="project-description">
+              A personal site about NBA legend Kareem Abdul Jabbar. Project
+              created by Eithan, mainly focusing on basic HTML & CSS.
+            </p>
+            <b-button href="https://web-development-environments-2023.github.io/208649913/" size="sm" class=" w-25" variant="success">View Project</b-button>
+          </div>
+        </div>
+      </div>
+    </div>
+<div class="together_projects mt-3 text-center mb-5 pb-3">
+  <div>
+    <b>Spaceships Attack Game</b>
+  </div>
+  <div>
+  </div>
+  <div>
+    <p class="mt-3">
+      "Spaceships Attack Game" is an exciting web game developed by our team, focused on HTML Canvas drawing.
+       Take control of a spaceship and battle against enemy ships, inspired by games like "Chicken Invaders".
+       Survive, shoot down enemies, and avoid their attacks. Progress through challenging levels, improve your skills, and achieve high scores.
+      The game features a login system for saving progress and competing on the leaderboard. Enjoy engaging gameplay, immersive visuals,
+       and thrilling space battles for players of all ages.
+
+    </p>
+    <b-button href="https://web-development-environments-2023.github.io/assignment2-208649913_324474857/#" size="sm" style="width: 150px;" variant="success">View Project</b-button>
+
+  </div>
+</div>
+  </div>
   
-  <script>
-  export default {
-    data() {
-      return {
-        teamMembers: [
-          {
-            id: 1,
-            name: "Eithan Markman",
-            role: "Full-Stack Developer",
-            description: "John is a skilled frontend developer with a passion for creating engaging user interfaces.",
-          },
-          {
-            id: 2,
-            name: "Ariel Dawidowicz",
-            role: "Full-Stack Developer",
-            description: "Jane is an experienced backend developer, proficient in designing efficient server-side architectures.",
-          },
-        ],
-        previousProjects: [
-          {
-            id: 1,
-            title: "Spaceships Attack Game",
-            description: "A web game project built by the team. Mainly focused on HTML Canvas element drawing. ",
-            link: "https://web-development-environments-2023.github.io/assignment2-208649913_324474857/#",
-          },
-          {
-            id: 2,
-            title: "Personal Site - Kareem Abdul Jabbar",
-            description: "A personal site about NBA legend Kareem Abdul Jabbar.\n Project created by Eithan, mainly focuces on basic HTML & CSS.",
-            link: "https://web-development-environments-2023.github.io/208649913/",
-          },
-          {
-            id: 3,
-            title: "Personal Site - ",
-            description: "A personal site about NBA legend Kareem Abdul Jabbar.\n Project created by Eithan, mainly focuces on basic HTML & CSS.",
-            link: "https://web-development-environments-2023.github.io/208649913/",
-          },
-        ],
-      };
-    },
-  };
-  </script>
-  
+</template>
+
+<script>
+export default {
+  name: "AboutPage",
+  data() {
+    return {
+      ariel: require("@/assets/ariel.png"),
+      eithan: require("@/assets/eithan.png"),
+    };
+  },
+};
+</script>
+
+<style scoped>
+#team_image {
+  width: 230px;
+  border-radius: 100%;
+  border: 7px solid #ffc107;
+  box-shadow: 0 0 25px 1px #00000063;
+}
+
+.card {
+  background-color: rgba(237, 240, 237, 0.567);
+  border-radius: 30px;
+  border: 5px solid rgba(91, 184, 91, 0.045);
+  padding-top: 20px;
+}
+
+#title {
+  color: rgb(91, 184, 91);
+  font-size: 35px;
+}
+
+.together_projects{
+  background-color: rgba(237, 240, 237, 0.567);
+  border-radius: 30px;
+  border: 5px solid rgba(91, 184, 91, 0.045);
+  padding-top: 20px;
+
+}
+</style>
